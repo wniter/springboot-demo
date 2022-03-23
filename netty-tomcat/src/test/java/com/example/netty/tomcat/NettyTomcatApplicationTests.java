4ab3d404511d7fa7f1af09a5df62632f11cc3e43
Boolean <lyg2000@aliyun.com>
2022-03-23 11:27:59 +0800

    add

---
+++ netty-tomcat/src/test/java/com/example/netty/tomcat/NettyTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netty.tomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyTomcatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
