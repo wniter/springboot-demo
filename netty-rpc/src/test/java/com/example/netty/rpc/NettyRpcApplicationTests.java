f0e4659923279682e7a9b44eeab4a97c6a071c76
Boolean <lyg2000@aliyun.com>
2022-03-23 09:48:07 +0800

    add

--- netty-rpc/src/test/java/com/example/netty/rpc/NettyRpcApplicationTests.java
+++ netty-rpc/src/test/java/com/example/netty/rpc/NettyRpcApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.netty.io;
+package com.example.netty.rpc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NettyIoApplicationTests {
+class NettyRpcApplicationTests {
 
     @Test
     void contextLoads() {
