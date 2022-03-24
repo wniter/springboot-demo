f12397beb193c20aa15f83a3daecc70d27299f5f
Boolean <lyg2000@aliyun.com>
2022-03-24 09:41:54 +0800

    add

--- netty-io/src/main/java/com/example/netty/pipeline/InPipeline.java
+++ netty-io/src/main/java/com/example/netty/pipeline/InPipeline.java
@@ -1,4 +1,101 @@
 package com.example.netty.pipeline;
 
+import com.example.common.util.Logger;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.embedded.EmbeddedChannel;
+import org.junit.Test;
+
+/**
+ * Pipeline入站处理流程
+ *为了完整地演示Pipeline入站处理流程，将新建三个极为简单的入站处理器，三个入站
+ * 处理器分别为：SimpleInHandlerA、SimpleInHandlerB、SimpleInHandlerC。在
+ * ChannelInitializer初始化处理器的initChannel方法中，把它们加入到流水线中。添加的顺序
+ * 为A → B→ C。
+ */
 public class InPipeline {
+    static class SimpleInHandlerA extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            Logger.info("入站处理器 A: 被回调 ");
+            super.channelRead(ctx, msg);
+        }
+    }
+    static class SimpleInHandlerB extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            Logger.info("入站处理器 B: 被回调 ");
+            super.channelRead(ctx, msg);
+        }
+    }
+    static class SimpleInHandlerC extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            Logger.info("入站处理器 C: 被回调 ");
+            super.channelRead(ctx, msg);
+        }
+    }
+
+    /**
+     * [main|InPipeline$SimpleInHandlerA.channelRead] |>  入站处理器 A: 被回调
+     * [main|InPipeline$SimpleInHandlerB.channelRead] |>  入站处理器 B: 被回调
+     * [main|InPipeline$SimpleInHandlerC.channelRead] |>  入站处理器 C: 被回调
+     */
+    @Test
+    public void testPipelineInBound() {
+        ChannelInitializer i = new ChannelInitializer<EmbeddedChannel>() {
+            protected void initChannel(EmbeddedChannel ch) {
+                ch.pipeline().addLast(new SimpleInHandlerA());
+                ch.pipeline().addLast(new SimpleInHandlerB());
+                ch.pipeline().addLast(new SimpleInHandlerC());
+
+            }
+        };
+        EmbeddedChannel channel = new EmbeddedChannel(i);
+        ByteBuf buf = Unpooled.buffer();
+        buf.writeInt(1);
+        //向通道写一个入站报文
+        channel.writeInbound(buf);
+        try {
+            Thread.sleep(Integer.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    static class SimpleInHandlerB2 extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            Logger.info("入站处理器 B: 被回调 ");
+            //不调用基类的channelRead, 终止流水线的执行
+//            super.channelRead(ctx, msg);
+        }
+    }
+
+    //测试流水线的截断
+    @Test
+    public void testPipelineCutting() {
+        ChannelInitializer i = new ChannelInitializer<EmbeddedChannel>() {
+            protected void initChannel(EmbeddedChannel ch) {
+                ch.pipeline().addLast(new SimpleInHandlerA());
+                ch.pipeline().addLast(new SimpleInHandlerB2());
+                ch.pipeline().addLast(new SimpleInHandlerC());
+
+            }
+        };
+        EmbeddedChannel channel = new EmbeddedChannel(i);
+        ByteBuf buf = Unpooled.buffer();
+        buf.writeInt(1);
+        //向通道写一个入站报文
+        channel.writeInbound(buf);
+        try {
+            Thread.sleep(Integer.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
