3ab1584f3294c3e291068424bb0b37b840c81938
Boolean <lyg2000@aliyun.com>
2022-03-28 14:13:47 +0800

    add

---
+++ springboot-shiro/src/test/java/com/example/springboot/shiro/SpringbootShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.shiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
