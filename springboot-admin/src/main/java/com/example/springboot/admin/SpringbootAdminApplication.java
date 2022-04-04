ace129afd2a66a22b17056d4da20501b6b6cc624
Boolean <lyg2000@aliyun.com>
2022-04-05 01:44:00 +0800

    add

--- springboot-admin/src/main/java/com/example/springboot/admin/SpringbootAdminApplication.java
+++ springboot-admin/src/main/java/com/example/springboot/admin/SpringbootAdminApplication.java
@@ -1,8 +1,9 @@
 package com.example.springboot.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableAdminServer
 @SpringBootApplication
 public class SpringbootAdminApplication {
 
