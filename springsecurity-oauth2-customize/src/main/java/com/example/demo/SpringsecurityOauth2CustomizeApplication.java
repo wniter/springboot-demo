ae87d979d7f6c8f67a8cdeafd21a0767c7da2b52
Boolean <lyg2000@aliyun.com>
2022-04-07 01:24:29 +0800

    add

---
+++ springsecurity-oauth2-customize/src/main/java/com/example/demo/SpringsecurityOauth2CustomizeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityOauth2CustomizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityOauth2CustomizeApplication.class, args);
+    }
+
+}
