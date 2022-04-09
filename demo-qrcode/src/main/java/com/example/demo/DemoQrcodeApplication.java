b54fb5714380b4cf306ce11e557e8b810c4057a0
Boolean <lyg2000@aliyun.com>
2022-04-09 23:24:11 +0800

    add

---
+++ demo-qrcode/src/main/java/com/example/demo/DemoQrcodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoQrcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoQrcodeApplication.class, args);
+    }
+
+}
